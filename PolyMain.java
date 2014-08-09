f22bb8ca5569e9c22b4f4232ee71122f81944a28
Zavatta <mike@zavattas.com>
2014-08-09 02:35:28 -0700

    Create PolyMain.java

---
+++ PolyMain.java
@@ -0,0 +1,16 @@
+public class PolyMain  
+{   
+	public static void main(String [] args)    
+	{     
+		Book [] library = new Book [5];      
+		library[0] = new Novel("Jaws", 300, 15);     
+		library[1] = new Almanac("New World ", 100, 50);     
+		library[2] = new Novel("Animal Farm", 250, 9);      
+		listBooks(library);    
+	}     
+	static void listBooks(Book [] library)    
+	{     
+		for (int i = 0; i < library.length && library[i] != null; i++)       
+		library[i].print();    
+	}     
+} 
