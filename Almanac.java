12d1f82515a0ebe72d337e743cffaf89891f6ef0
Zavatta <mike@zavattas.com>
2014-08-09 02:36:05 -0700

    Create Almanac.java

---
+++ Almanac.java
@@ -0,0 +1,31 @@
+public class Almanac extends Book  
+{   
+	private int charts;      
+	public Almanac(String title, int pages, int charts)    
+	{     
+		super(title, pages);     
+		setCharts(charts);   
+	}    
+	public boolean setCharts(int charts)    
+	{     
+		if(charts > getPages())      
+		{       
+			System.err.println("Too many charts");       
+		return false;     
+		} 
+	else 
+		{       
+		this.charts = charts;       
+		return true;     
+		}   
+	}     
+	public int getCharts()    
+	{      
+	return charts; 
+	}    
+	public void print()    
+	{     
+	super.print();     
+	System.out.println(" and " + charts + " charts");   
+	} 
+}  
