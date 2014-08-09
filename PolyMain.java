01c1ccc4632943b6f08e1fe9df6d98c5a979280b
Zavatta <mike@zavattas.com>
2014-08-09 02:37:27 -0700

    Update PolyMain.java

--- PolyMain.java
+++ PolyMain.java
@@ -5,7 +5,8 @@ public class PolyMain
 		Book [] library = new Book [5];      
 		library[0] = new Novel("Jaws", 300, 15);     
 		library[1] = new Almanac("New World ", 100, 50);     
-		library[2] = new Novel("Animal Farm", 250, 9);      
+		library[2] = new Novel("Animal Farm", 250, 9); 
+		library[3] = new Phone("Dex", 500, 100);
 		listBooks(library);    
 	}     
 	static void listBooks(Book [] library)    
