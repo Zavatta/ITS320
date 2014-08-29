6da9aaea7623967603421610df06c213e49043ba
Zavatta <mike@zavattas.com>
2014-08-28 18:31:32 -0700

    Create Verify.java

---
+++ Verify.java
@@ -0,0 +1,17 @@
+//Filename Verify.java
+//Written by Mike Zavatta-MacDonald
+//Written on 8/2/2014
+
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+
+public class Verify 
+{
+	private int minimum = 10;
+//declare minimum value	
+	private int maximum = 100;
+//declare maximum value
+	{
+	}
+}
+	
