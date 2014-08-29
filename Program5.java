6767afce2eb63f394436f2d299d4acee4784cd62
Zavatta <mike@zavattas.com>
2014-08-28 22:03:45 -0700

    Update Program5.java

--- Program5.java
+++ Program5.java
@@ -9,3 +9,19 @@ public class Program5
 	{
 	public static void main(String[] args)
 		{
+		// Create a Scanner object for keyboard input.
+		Scanner keyboard = new Scanner(System.in);
+		// Get the starting number.
+		int number; 
+		Verify testnum;
+		System.out.print("Enter a number between 10 and 100.");
+		number = keyboard.nextInt();
+		testnum = new Verify(10,100);
+		try
+			{testnum.Validate(number);
+			}
+		catch
+			{
+			}
+		}
+	}
