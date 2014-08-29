f2926b43daa19e1e582c9b394c525129c3597daf
Zavatta <mike@zavattas.com>
2014-08-28 20:24:06 -0700

    Create Program5.java

---
+++ Program5.java
@@ -0,0 +1,11 @@
+//Filename Program5.java
+//Written by Mike Zavatta-MacDonald
+//Written on 8/27/2014
+
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+
+public class Program5
+	{
+	public static void main(String[] args)
+		{
