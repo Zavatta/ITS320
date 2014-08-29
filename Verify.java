2fdfcda674811de7651c15d108dd858237481014
Zavatta <mike@zavattas.com>
2014-08-28 19:52:35 -0700

    Update Verify.java

--- Verify.java
+++ Verify.java
@@ -5,13 +5,41 @@
 import javax.swing.JOptionPane;
 import java.util.Scanner;
 
-public class Verify 
+public class Verify
 {
-	private int minimum = 10;
-//declare minimum value	
-	private int maximum = 100;
-//declare maximum value
-	{
-	}
+int min,max;
+public Verify(int min,int max)
+{
+this.min = min;
+this.max = max;
 }
-	
+public void Validate(int number) throws Exception
+{
+
+if(number > 0 && number < min)
+{
+throw new NumberLowException("NumberLowException: number < 10");
+//throw NumberLowException
+}
+
+if(number > max)
+{
+throw new NumberHighException("NumberHighException: number > 100");
+//throw NumberHighException
+}
+
+if(number < 0)
+{
+throw new NumberNegativeException("NumberNegativeException: number < 0");
+//Throw NumberNegativeException
+}
+
+
+if(number >= min && number <= max)
+{
+System.out.println("is a valid number");
+//if not exception print valid number
+}
+
+}
+}	
